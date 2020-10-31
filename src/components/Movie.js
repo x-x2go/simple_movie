59c35f7ce21862f29619a1f5551f150a191988d9
x-x2go <xxxvi_@naver.com>
2020-10-31 22:50:41 +0900

    Add router and Navigation

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }){
-    return (<div className="movie">
+    return (
+    <div className="movie">
+        <Link to={{ pathname: `/movie/${id}`, state:{
+            year, title, summary, poster, genres
+        }}}>
         <img src={poster} alt={title} title={title}/>
         <div className="movie__data">
             <h3 className="movie__title">{title}</h3>
@@ -11,7 +16,9 @@ function Movie({ id, year, title, summary, poster, genres }){
             <ul className="movie__genres">{genres.map((genre, index)=><li key={index} className="genres__genre">{genre}</li>)}</ul>
             <p className="movie__summary">{summary.slice(0, 140)}...</p>
         </div>
-    </div>)
+    </Link>
+    </div>
+    )
 }
 
 Movie.propTypes = {
